84a259007a1d27a72277c3afaf51e95f1770967d
Dimitri Fontaine <dim@tapoueh.org>
2014-08-22 00:44:04 +0200

    Some More Cleanup.

--- src/front-main.lisp
+++ src/front-main.lisp
@@ -3,18 +3,6 @@
 ;;;
 ;;; Main dashboard
 ;;;
-(defun front-dashboard ()
-  "NIY"
-  (serve-page
-   (with-html-output-to-string (s)
-     (htm
-      (:div :class "col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main"
-            (:h1 "pgcharts dashboard")
-            (:p "Soon, a nice dashboard showing the list of databases you
-            did setup to be able to work with pgcharts, and maybe some
-            direct link to most recently used queries and graphs and things
-            like that.") )))))
-
 (defun front-list-queries ()
   "Serve the list of SQL queries."
   (let ((query-list (with-pgsql-connection (*dburi*)
