4c92118549a82ae46f17920f787995d8a9999ce2
Dimitri Fontaine <dim@tapoueh.org>
2015-09-03 16:30:36 +0200

    Improve debian packaging of pgcharts.
    Create a pgcharts system user and log and pid directories where to
    handle our files, also add a good default configuration file in /etc,
    and overrides lintian with comments when needed.

--- debian/settings.lisp
+++ debian/settings.lisp
@@ -1,4 +1,5 @@
 (in-package #:pgcharts)
 
-(setf *pidfile* "~/.pgcharts.pid")
-(setf *logfile* "~/.pgcharts.log")
+(setf *config-filename* "/etc/pgcharts.ini")
+(setf *pidfile* "/var/run/pgcharts.pid")
+(setf *logfile* "/var/log/pgcharts.log")
